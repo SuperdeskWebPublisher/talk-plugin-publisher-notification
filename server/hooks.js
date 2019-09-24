aa51471c966daeeef16b7c88786925fb03139043
Tomasz Rondio <tomasz.rondio@gmail.com>
2019-09-24 09:07:55 +0200

    data format change for publisher 2.0

--- server/hooks.js
+++ server/hooks.js
@@ -1,28 +1,28 @@
-const fetch = require('node-fetch');
-const AssetModel = require('../../../models/asset');
-const CommentsLoader = require('../../../graph/loaders/comments.js');
+const fetch = require("node-fetch");
+const AssetModel = require("../../../models/asset");
+const CommentsLoader = require("../../../graph/loaders/comments.js");
 
 const sendData = (asset, commentCount) => {
   process.nextTick(async () => {
-    const response = await fetch(process.env.TALK_PUBLISHER_API_URL + 'content/articles', {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        'x-superdesk-signature': 'sha1=' + process.env.TALK_PUBLISHER_AUTH_TOKEN
-      },
-      body: JSON.stringify({
-        article_comments: {
+    const response = await fetch(
+      process.env.TALK_PUBLISHER_API_URL + "content/articles",
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          "x-superdesk-signature":
+            "sha1=" + process.env.TALK_PUBLISHER_AUTH_TOKEN
+        },
+        body: JSON.stringify({
           url: asset.url,
           commentsCount: commentCount
-        }
-      }),
-    });
+        })
+      }
+    );
     if (!response.ok) {
       const responseText = await response.text();
       console.trace(
-        `Posting comment count to Publisher failed with HTTP code ${
-          response.status
-        } and body '${responseText}'`
+        `Posting comment count to Publisher failed with HTTP code ${response.status} and body '${responseText}'`
       );
     }
   });
@@ -35,24 +35,26 @@ module.exports = {
         const asset = await AssetModel.findOne({
           id: result.comment.asset_id
         });
-        const commentCount = await CommentsLoader().Comments.countByAssetID.load(result.comment.asset_id);
+        const commentCount = await CommentsLoader().Comments.countByAssetID.load(
+          result.comment.asset_id
+        );
 
         sendData(asset, commentCount);
         return result;
-      },
+      }
     },
     setCommentStatus: {
       async post(root, args, ctx) {
         const comment = await ctx.loaders.Comments.get.load(args.id);
         if (comment) {
-          const asset = await ctx.loaders.Assets.getByID.load(
+          const asset = await ctx.loaders.Assets.getByID.load(comment.asset_id);
+          const commentCount = await CommentsLoader().Comments.countByAssetID.load(
             comment.asset_id
           );
-          const commentCount = await CommentsLoader().Comments.countByAssetID.load(comment.asset_id);
 
           sendData(asset, commentCount);
         }
       }
-    },
-  },
+    }
+  }
 };
